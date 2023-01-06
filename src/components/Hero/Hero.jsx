76f869bfee3b482b8b14c55d1e0cce4b02b9c819
arielcazzulino <arielcazzulino@gmail.com>
2023-01-05 21:42:22 -0300

    pre change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import img from "../../images/profile.jpg"
 import Styles from "./Hero.module.css"
+import github from '../../images/github.png'
+import linkedin from '../../images/linkedin.png'
+import email from '../../images/email.png'
 
 const Hero = () =>{
     return (
@@ -9,6 +12,11 @@ const Hero = () =>{
             <div>
                 <h1 className={Styles.h1}>I'm <span className={Styles.name}>Ariel</span> Cazzulino</h1>
                 <p className={Styles.devscription}>Fullstack Developer</p>
+                <div>
+                    <img src={linkedin} alt="Linkedin" className={Styles.socialIcon}/>
+                    <img src={github} alt="Github" className={Styles.socialIcon}/>
+                    <img src={email} alt="Email" className={Styles.socialIcon}/>
+                </div>
             </div>
         </div>
         <div className={Styles.div2}>
